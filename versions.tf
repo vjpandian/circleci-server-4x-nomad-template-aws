66218dfd63a79967794d16912a5c2c9de026acb1
Vijay Pandian <ananth.vijay@gmail.com>
2023-10-13 11:55:24 -0400

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,18 @@
+terraform {
+   backend "s3" {
+   bucket = "<s3-bucket-for-storing-state" # replace this with a new bucket for persisting tf state
+   key    = "<tf-state-folder"
+   region = "<aws-region-for-tf-state-bucket>"
+  }
+  required_version = ">= 0.15.4"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">=4.4.0"
+    }
+     cloudinit = {
+      version = ">=2.0"
+      source  = "hashicorp/cloudinit"
+    }
+  }
+}
